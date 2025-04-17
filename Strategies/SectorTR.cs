e320f81c441e10140c6d81446cde52a59cbd34b9
gulopez <gulopez@outlook.com>
2025-04-17 08:03:22 -0500

    Updatingexit strategy lon entries

--- Strategies/SectorTR.cs
+++ Strategies/SectorTR.cs
@@ -20,100 +20,86 @@ using NinjaTrader.NinjaScript;
 using NinjaTrader.Core.FloatingPoint;
 using NinjaTrader.NinjaScript.Indicators;
 using NinjaTrader.NinjaScript.DrawingTools;
-using System.IO;
+using NinjaTrader.NinjaScript.Utilities;
 #endregion
 
 //This namespace holds Strategies in this folder and is required. Do not change it. 
 namespace NinjaTrader.NinjaScript.Strategies
 {
-	public class SectorTR : Strategy
-	{
+    public class TRSector : Strategy
+    {
+
         private bool _initialized = false;
-        bool GuiaArriba = false;
+
         private DireccionTR DireccionTR2Min;
         private DireccionTR DireccionTR12Min;
         private GuiaTR GuiaTR1;
         private SMA sma;
-		private EMA ema2;
+        private EMA ema2;
         private EMA ema15;
-        private string FileNamePrefix = "Sector";
-        private string FileName = "";
-        private string HeaderText = "Time, BarInProgress,Open,Close,High,Low,Guia,GuiaArriba,IsHighGTGuia, IsLowCrossGuia,IsFacturoGuia, IsFacturoLTGuia,sma50, ema2, ema15,IsCloseGTSMA50, IsEma2Rising,IsEma15Rising, IsCloseGTema2,IsSectorAlcista,IsSectorBajista";
-        private string docPath = "";
-        //private ATR atr;
-        private string path;
-        private StreamWriter sw; // a variable for the StreamWriter that will be used 
 
+        private TRProperties trProp = new TRProperties();
+
+        private double StopPrice;
+        private double TriggerPrice;
+        private int TriggerState;
+
+
+        //private double _StopPrice;
+        //private double _TriggerPrice;
 
         protected override void OnStateChange()
-		{
-			if (State == State.SetDefaults)
-			{
-				Description									= @"Testing Entrada Sector";
-				Name										= "SectorTR";
-				Calculate									= Calculate.OnBarClose;
-				EntriesPerDirection							= 1;
-				EntryHandling								= EntryHandling.AllEntries;
-				IsExitOnSessionCloseStrategy				= true;
-				ExitOnSessionCloseSeconds					= 30;
-				IsFillLimitOnTouch							= false;
-				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
-				OrderFillResolution							= OrderFillResolution.Standard;
-				Slippage									= 0;
-				StartBehavior								= StartBehavior.WaitUntilFlat;
-				TimeInForce									= TimeInForce.Gtc;
-				TraceOrders									= false;
-				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
-				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
-				BarsRequiredToTrade							= 20;
-                IsWriteToFile = true;
-                
+        {
+            if (State == State.SetDefaults)
+            {
+                Description = @"TR Sector Strategy";
+                Name = "TRSector";
+                Calculate = Calculate.OnBarClose;
+                EntriesPerDirection = 1;
+                EntryHandling = EntryHandling.AllEntries;
+                IsExitOnSessionCloseStrategy = true;
+                ExitOnSessionCloseSeconds = 30;
+                IsFillLimitOnTouch = false;
+                MaximumBarsLookBack = MaximumBarsLookBack.TwoHundredFiftySix;
+                OrderFillResolution = OrderFillResolution.Standard;
+                Slippage = 0;
+                StartBehavior = StartBehavior.WaitUntilFlat;
+                TimeInForce = TimeInForce.Gtc;
+                TraceOrders = false;
+                RealtimeErrorHandling = RealtimeErrorHandling.StopCancelClose;
+                StopTargetHandling = StopTargetHandling.PerEntryExecution;
+                BarsRequiredToTrade = 20;
                 // Disable this property for performance gains in Strategy Analyzer optimizations
                 // See the Help Guide for additional information
-                IsInstantiatedOnEachOptimizationIteration	= true;
+                IsInstantiatedOnEachOptimizationIteration = true;
 
-                FileName = FileNamePrefix + DateTime.Now.ToString() + ".csv";
-                path = NinjaTrader.Core.Globals.UserDataDir + FileName; // Define the Path to our test file
+                BreakEvenTrigger = 5;
+                InitialStopDistance = -10;
+                StopPrice = 0;
+                TriggerPrice = 0;
+                TriggerState = 0;
+
+
             }
-			else if (State == State.Configure)
-			{
+            else if (State == State.Configure)
+            {
                 AddDataSeries(Data.BarsPeriodType.Minute, 5);
                 AddDataSeries(Data.BarsPeriodType.Minute, 15);
-
-                
-                if (IsWriteToFile)
-                {
-                    //sw = File.AppendText(path);  // Open the path for writing
-                    //sw.WriteLine(HeaderText + Environment.NewLine); // Append a new line to the file
-                    //sw.Close(); // Close the file to allow future calls to access the file again.
-                }
             }
-			else if (State == State.DataLoaded)
-			{
+            else if (State == State.DataLoaded)
+            {
                 DireccionTR2Min = DireccionTR(Closes[0], 10, 20, 6, 35, 50, 70);
                 sma = SMA(BarsArray[0], 50);
                 // atr = ATR(BarsArray[0], 5);
 
                 ema2 = EMA(BarsArray[0], 20);
                 ema15 = EMA(BarsArray[2], 20);
-
                 GuiaTR1 = GuiaTR(Closes[1], 5, 2.1);
-
-               
-            }
-            else if (State == State.Terminated)
-            {
-                //if (sw != null)
-                //{
-                //    sw.Close();
-                //    sw.Dispose();
-                //    sw = null;
-                //}
             }
         }
 
-		protected override void OnBarUpdate()
-		{
+        protected override void OnBarUpdate()
+        {
             //Make sure Daily bars are greater than BarsRequiredToTrade
             if (BarsInProgress == 2 && CurrentBar > BarsRequiredToTrade)
                 _initialized = true;
@@ -121,81 +107,177 @@ namespace NinjaTrader.NinjaScript.Strategies
             if (!_initialized)
                 return;
 
-            //Calculate Guia
-            if (!GuiaArriba && GuiaTR1[0] > High[0] && GuiaTR1[0] > GuiaTR1[1])
+
+            CalculateSectorSignals();
+
+            if (BarsInProgress == 0)
             {
-                GuiaArriba = true;
-            }
-            else
-            {
-                if (GuiaArriba && GuiaTR1[0] < Low[0] && GuiaTR1[0] < GuiaTR1[1])
-                    GuiaArriba = false;
-            }
+                ProcessSectorSignals();
 
-            bool IsHighGTGuia = GuiaTR1[0] < High[0];
-			bool IsLowCrossGuia = GuiaTR1[0] > Low[0];
-			bool IsFacturoGuia = Close[0] > GuiaTR1[0];
-			bool IsFacturoLTGuia = Close[0] < GuiaTR1[0];
-
-			bool IsCloseGTSMA50Alcista = (Close[0] > sma[0] && Open[0] < sma[0]) ? true : false;
-            bool IsCloseGTSMA50Bajista = (Close[0] < sma[0] && Open[0] > sma[0]) ? true : false;
-
-            bool IsEma2Rising = (ema2[0] > ema2[1]) ? true : false;
-            bool IsEma15Rising = (ema15[0] > ema15[1]) ? true : false;
-
-			bool IsCloseGTEma2min = (Close[0] > ema2[0]) ? true : false;
-
-			bool IsSectorAlcista = false;
-            bool IsSectorBajista = false;
-
-
-            if (!GuiaArriba && IsEma2Rising && !IsEma15Rising && IsCloseGTSMA50Alcista)
-			{
-				IsSectorAlcista = true;
+                BreakEvenExtiStrategy();
 
             }
 
-            if (GuiaArriba && !IsEma2Rising && IsEma15Rising && IsCloseGTSMA50Bajista)
-            {
-                IsSectorBajista = true;
-
-            }
-
-            if(BarsInProgress == 0)
-            {
-
-                if (IsSectorAlcista || IsSectorBajista)
-                {
-                    _ = Draw.Dot(this, @"SectorTR"+CurrentBar, true, 0, High[0] + 5, Brushes.CornflowerBlue);
-
-                    string logEntry = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20}", Time[0], BarsInProgress, Open[0], Close[0], High[0], Low[0], GuiaTR1[0], GuiaArriba, IsHighGTGuia, IsLowCrossGuia, IsFacturoGuia, IsFacturoLTGuia, sma[0], ema2[0], ema15[0], IsCloseGTSMA50Alcista, IsEma2Rising, IsEma15Rising, IsCloseGTEma2min, IsSectorAlcista, IsSectorBajista);
-                    Print(logEntry);
-
-                    if (IsWriteToFile)
-                    {
-
-                       
-                        // Append new lines of text to the file                        //sw = File.AppendText(path);  // Open the path for writing
-                        //sw.WriteLine(logEntry); // Append a new line to the file
-                        //sw.Close(); // Close the file to allow future calls to access the file again.
-
-
-                        // Time, BarInProgress,Open,Close,High,Low,Guia,GuiaArriba,IsHighGTGuia, IsLowCrossGuia,IsFacturoGuia, IsFacturoLTGuia,sma50, ema2, ema15,IsCloseGTSMA50, IsEma2Rising,IsEma15Rising, IsCloseGTema2,IsSectorAlcista,IsSectorBajista
-                       
-
-                    }
-                }
-            }
         }
 
 
+        private void CalculateSectorSignals()
+        {
+            // Calculate Guia
+            if (!trProp.GuiaArriba && GuiaTR1[0] > High[0] && GuiaTR1[0] > GuiaTR1[1])
+            {
+                trProp.GuiaArriba = true;
+            }
+            else
+            {
+                if (trProp.GuiaArriba && GuiaTR1[0] < Low[0] && GuiaTR1[0] < GuiaTR1[1])
+                    trProp.GuiaArriba = false;
+            }
+
+            trProp.IsHighGTGuia = GuiaTR1[0] < High[0];
+            trProp.IsLowCrossGuia = GuiaTR1[0] > Low[0];
+            trProp.IsFacturoGuia = Close[0] > GuiaTR1[0];
+            trProp.IsFacturoLTGuia = Close[0] < GuiaTR1[0];
+
+            trProp.IsCloseGTSMA50Alcista = (Close[0] > sma[0] && Open[0] < sma[0]) ? true : false;
+            trProp.IsCloseGTSMA50Bajista = (Close[0] < sma[0] && Open[0] > sma[0]) ? true : false;
+
+            trProp.IsEma2Rising = (ema2[0] > ema2[1]) ? true : false;
+            trProp.IsEma15Rising = (ema15[0] > ema15[1]) ? true : false;
+            trProp.IsEma2OverEma15 = (ema2[0] > ema15[0]) ? true : false;
+
+            trProp.IsCloseGTEma2min = (Close[0] > ema2[0]) ? true : false;
+
+            trProp.IsSectorAlcista = false;
+            trProp.IsSectorBajista = false;
+
+            if (!trProp.GuiaArriba && !trProp.IsEma2OverEma15 && trProp.IsEma2Rising && !trProp.IsEma15Rising && trProp.IsCloseGTSMA50Alcista)
+            {
+                trProp.IsSectorAlcista = true;
+            }
+
+            if (trProp.GuiaArriba && trProp.IsEma2OverEma15 && !trProp.IsEma2Rising && trProp.IsEma15Rising && trProp.IsCloseGTSMA50Bajista)
+            {
+                trProp.IsSectorBajista = true;
+            }
+        }
+
+        private void ProcessSectorSignals()
+        {
+            if (trProp.IsSectorAlcista == true || trProp.IsSectorBajista == true)
+            {
+                string entradamessage = "";
+                if (trProp.IsSectorAlcista == true)
+                {
+                    entradamessage = string.Format("{0}, Sector Alcista", Time[0]);
+                    //  TRUtilities.SaveToFile(_Path, IsWriteToFile, entradamessage);
+                    PrintOutput(true, entradamessage);
+
+                    Draw.Dot(this, @"SectorTR" + CurrentBar, true, 0, Low[0] - 1, Brushes.CornflowerBlue);
+                    Draw.Text(this, "tag1" + CurrentBar, "Sector Alcista", 0, Convert.ToInt32(Low[0]) - 10, ChartControl.Properties.ChartText);
+
+                    StopPrice = Low[0];
+                    TriggerPrice = ema15[0];
+
+                    if (Position.MarketPosition == MarketPosition.Flat)
+                    {
+                        TriggerState = 1;
+                        PrintOutput("Entering Long");
+                        EnterLong(Convert.ToInt32(DefaultQuantity), @"entry");
+                    }
+                }
+                else
+                {
+                    //entradamessage = string.Format("{0}, Sector Bajista", Time[0]);
+                    //  TRUtilities.SaveToFile(_Path, IsWriteToFile, entradamessage);
+                    //PrintOutput(true, entradamessage);
+
+                    //Draw.Dot(this, @"SectorTR" + CurrentBar, true, 0, High[0] + 1, Brushes.CornflowerBlue);
+                    //Draw.Text(this, "tag1" + CurrentBar, "Sector Bajista", 0, Convert.ToInt32(High[0]) + 10, ChartControl.Properties.ChartText);
+
+                    //_StopPrice = High[0] + 1;
+                    //_TriggerPrice = ema15[0];
+
+                }
+
+                //string logEntry = string.Format(BodyText, Time[0], BarsInProgress, Open[0], Close[0], High[0], Low[0], GuiaTR1[0], trProp.GuiaArriba, trProp.IsHighGTGuia, trProp.IsLowCrossGuia, trProp.IsFacturoGuia, trProp.IsFacturoLTGuia, sma[0], ema2[0], ema15[0], trProp.IsCloseGTSMA50Alcista, trProp.IsEma2Rising, trProp.IsEma15Rising, trProp.IsCloseGTEma2min, trProp.IsSectorAlcista, trProp.IsSectorBajista);
+                // TRUtilities.SaveToFile(_Path, (IsWriteToFile && IsVerboseLogs), logEntry);
+            }
+        }
+
+        private void PrintOutput(string text)
+        {
+
+            Print(string.Format("{0},{1},{2},{3}", Time[0], text, TriggerPrice, StopPrice));
+
+        }
+        private void BreakEvenExtiStrategy()
+        {
+
+
+            // Set 1
+            if ((TriggerState >= 2)
+                 && (Position.MarketPosition == MarketPosition.Flat))
+            {
+                TriggerState = 0;
+            }
+
+            // Set 3
+            if ((TriggerState == 1)
+                 && (Position.MarketPosition == MarketPosition.Long))
+            {
+                TriggerState = 2;
+
+                // StopPrice = Low[0] - 1;
+
+                //PrintOutput("Long Updating Stop and Target");
+            }
+
+            // Set 4
+            if ((TriggerState == 2)
+                 && (Close[0] >= TriggerPrice))
+            {
+                TriggerState = 3;
+
+                StopPrice = Low[0];
+
+                // PrintOutput("Resetting Stop");
+                Draw.Diamond(this, @"BreakEvenBuilderExample Diamond_1", true, 0, (High[0] + (2 * TickSize)), Brushes.DarkCyan);
+            }
+
+            // Set 5
+            if (TriggerState >= 2)
+            {
+                if (TriggerState == 3)
+                {
+                    StopPrice = Low[0];
+                }
+
+                //  PrintOutput("Updating the Stop Exit");
+                ExitLongStopMarket(Convert.ToInt32(DefaultQuantity), StopPrice, @"exit", @"entry");
+            }
+
+        }
+        private void PrintOutput(bool Verbose, string text)
+        {
+            if (Verbose)
+            {
+                Print(text);
+            }
+        }
+
         #region Properties
         [NinjaScriptProperty]
-        [Display(Name = "IsWriteToFile", Description = "Log entries to a file", Order = 1, GroupName = "Parameters")]
-        public bool IsWriteToFile
+        [Range(1, int.MaxValue)]
+        [Display(Name = "BreakEvenTrigger", Description = "Number of ticks above entry the breakeven movement trigger is set", Order = 1, GroupName = "Parameters")]
+        public int BreakEvenTrigger
         { get; set; }
 
-       
+        [NinjaScriptProperty]
+        [Range(-999, int.MaxValue)]
+        [Display(Name = "InitialStopDistance", Description = "(use a negative) Number of ticks from entry the stop will initially be placed below", Order = 2, GroupName = "Parameters")]
+        public int InitialStopDistance
+        { get; set; }
         #endregion
     }
 }
